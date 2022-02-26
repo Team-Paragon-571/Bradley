8eeeff5f474275f6047516c1777d7496dba6a0d6
NPSYCH02 <cottonaomi3@gmail.com>
2022-02-26 14:59:08 -0500

    Add a shoot command to the robot code.
    * Add the left and right index motor to the index
    * Add stop motor command to shooter
    * Add shoot motor to constanrs

--- src/main/java/org/frc571/bradley/commands/ShootCommand.java
+++ src/main/java/org/frc571/bradley/commands/ShootCommand.java
@@ -23,6 +23,7 @@ public class ShootCommand extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        m_shoot.shoot();
     }
 
     // Called once the command ends or is interrupted.
