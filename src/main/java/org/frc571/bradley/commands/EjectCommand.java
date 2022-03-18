c2c4632ce0701bc8b29bb77704c33df48698c3ed
Naomi Cotto <cottonaomi3@gmail.com>
2022-03-18 19:39:57 -0400

    fix more warnings

--- src/main/java/org/frc571/bradley/commands/EjectCommand.java
+++ src/main/java/org/frc571/bradley/commands/EjectCommand.java
@@ -1,7 +1,6 @@
 package org.frc571.bradley.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class EjectCommand extends ParallelCommandGroup {
     public EjectCommand() {
