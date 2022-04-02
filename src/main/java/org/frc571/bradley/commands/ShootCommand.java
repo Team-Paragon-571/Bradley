d1a0dbec37e9f90696f4bd7961225b5500b25975
fishyfishfish55 <notthejeans55@gmail.com>
2022-04-02 11:44:15 -0400

    Remove empty documentation comment

--- src/main/java/org/frc571/bradley/commands/ShootCommand.java
+++ src/main/java/org/frc571/bradley/commands/ShootCommand.java
@@ -6,19 +6,15 @@ import org.frc571.bradley.Constants.TimeConstants;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-    // revButton.whenPressed(new ReverseHopperCommand().withTimeout(0.1), true);
+// revButton.whenPressed(new ReverseHopperCommand().withTimeout(0.1), true);
 public class ShootCommand extends ParallelCommandGroup {
-    /**
-     *
-     */
 
-    public ShootCommand () {
+    public ShootCommand() {
         addCommands(
-            new RaiseIntake(),
-            new WaitCommand(TimeConstants.BACKUP_TIME).andThen(new RevCommand(SpeedConstants.FLYWHEEL_MAX_SPEED)), 
-            new ReverseHopperCommand().withTimeout(TimeConstants.FLYWHEEL_REV_TIME)
-                .andThen(new FireCommand())
-        );
+                new RaiseIntake(),
+                new WaitCommand(TimeConstants.BACKUP_TIME).andThen(new RevCommand(SpeedConstants.FLYWHEEL_MAX_SPEED)),
+                new ReverseHopperCommand().withTimeout(TimeConstants.FLYWHEEL_REV_TIME)
+                        .andThen(new FireCommand()));
     }
-    
+
 }
