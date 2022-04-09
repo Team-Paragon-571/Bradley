6c0e19d3e0d2f6f372d3c1a33ec26af46521c250
NPSYCH02 <cottonaomi3@gmail.com>
2022-04-09 09:51:53 -0400

    Set correct auto speed

--- src/main/java/org/frc571/bradley/commands/AutonomousShootDriveCommand.java
+++ src/main/java/org/frc571/bradley/commands/AutonomousShootDriveCommand.java
@@ -1,6 +1,7 @@
 package org.frc571.bradley.commands;
 
 import org.frc571.bradley.Constants.AutonomousConstants;
+import org.frc571.bradley.Constants.SpeedConstants;
 
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -10,7 +11,7 @@ public class AutonomousShootDriveCommand extends SequentialCommandGroup {
         public AutonomousShootDriveCommand(double driveTimeout) {
                 addCommands(
                                 new ParallelRaceGroup(
-                                new RevCommand(1).withTimeout(2),
+                                new RevCommand(SpeedConstants.FLYWHEEL_MAX_SPEED).withTimeout(2),
                                 new WaitCommand(0.2).andThen(new FireCommand().withTimeout(1))),
                                 new AutonomousDriveCommand()
                                                 .withTimeout(AutonomousConstants.AUTONOMOUS_COMMAND_DURATION));
