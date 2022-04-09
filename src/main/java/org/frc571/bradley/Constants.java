e1c2bc62aa9b1ba7f3c4113f2bb68a2e1c006997
NPSYCH02 <cottonaomi3@gmail.com>
2022-04-09 18:25:09 -0400

    raise intake motor speed

--- src/main/java/org/frc571/bradley/Constants.java
+++ src/main/java/org/frc571/bradley/Constants.java
@@ -55,7 +55,7 @@ public class Constants {
     }
 
     public static final class SpeedConstants {
-        public static final double kIntakeMotorSpeed = 0.50;
+        public static final double kIntakeMotorSpeed = 0.75;
         public static final double RAISE_INTAKE_SPEED = 0.3;
         public static final double FLYWHEEL_MAX_SPEED = 0.79;
     }
